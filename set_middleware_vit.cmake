4ee5885b4d18535d94ea11af9e9a523ab9bbd855
mcu_sdk.ci <mcu_sdk.ci@nxp.com>
2024-09-24 14:30:52 +0000

    Add files generated at mcu-sdk-2.0 468009a35d
    Signed-off-by: mcu_sdk.ci <mcu_sdk.ci@nxp.com>

--- set_middleware_vit.cmake
+++ set_middleware_vit.cmake
@@ -1,110 +1,12 @@
 include_guard(GLOBAL)
 
 
-if (CONFIG_USE_middleware_vit_hifi4-rt685)
-# Add set(CONFIG_USE_middleware_vit_hifi4-rt685 true) in config.cmake to use this component
-
-message("middleware_vit_hifi4-rt685 component is included from ${CMAKE_CURRENT_LIST_FILE}.")
-
-if(CONFIG_USE_middleware_vit_hifi4-rt685_models AND (CONFIG_BOARD STREQUAL evkmimxrt685 OR CONFIG_BOARD STREQUAL mimxrt685audevk))
-
-target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
-  ${CMAKE_CURRENT_LIST_DIR}/./RT600-HIFI4/Lib
-  ${CMAKE_CURRENT_LIST_DIR}/./RT600-HIFI4/Lib/Inc
-)
-
-if(CONFIG_TOOLCHAIN STREQUAL xcc)
-  target_link_libraries(${MCUX_SDK_PROJECT_NAME} PRIVATE
-    -Wl,--start-group
-      ${CMAKE_CURRENT_LIST_DIR}/./RT600-HIFI4/Lib/libVIT_HIFI4_v04_10_00.a
-      -Wl,--end-group
-  )
-endif()
-
-else()
-
-message(SEND_ERROR "middleware_vit_hifi4-rt685 dependency does not meet, please check ${CMAKE_CURRENT_LIST_FILE}.")
-
-endif()
-
-endif()
-
-
-if (CONFIG_USE_middleware_vit_hifi4-rt685_models)
-# Add set(CONFIG_USE_middleware_vit_hifi4-rt685_models true) in config.cmake to use this component
-
-message("middleware_vit_hifi4-rt685_models component is included from ${CMAKE_CURRENT_LIST_FILE}.")
-
-if((CONFIG_BOARD STREQUAL evkmimxrt685 OR CONFIG_BOARD STREQUAL mimxrt685audevk))
-
-target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
-  ${CMAKE_CURRENT_LIST_DIR}/./RT600-HIFI4/Lib
-)
-
-else()
-
-message(SEND_ERROR "middleware_vit_hifi4-rt685_models dependency does not meet, please check ${CMAKE_CURRENT_LIST_FILE}.")
-
-endif()
-
-endif()
-
-
-if (CONFIG_USE_middleware_vit_fusionf1)
-# Add set(CONFIG_USE_middleware_vit_fusionf1 true) in config.cmake to use this component
-
-message("middleware_vit_fusionf1 component is included from ${CMAKE_CURRENT_LIST_FILE}.")
-
-if(CONFIG_USE_middleware_vit_fusionf1_models AND (CONFIG_BOARD STREQUAL evkmimxrt595))
-
-target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
-  ${CMAKE_CURRENT_LIST_DIR}/./FusionF1/Lib
-  ${CMAKE_CURRENT_LIST_DIR}/./FusionF1/Lib/Inc
-)
-
-if(CONFIG_TOOLCHAIN STREQUAL xcc)
-  target_link_libraries(${MCUX_SDK_PROJECT_NAME} PRIVATE
-    -Wl,--start-group
-      ${CMAKE_CURRENT_LIST_DIR}/./FusionF1/Lib/libVIT_Fusion_F1_v04_10_00.a
-      -Wl,--end-group
-  )
-endif()
-
-else()
-
-message(SEND_ERROR "middleware_vit_fusionf1 dependency does not meet, please check ${CMAKE_CURRENT_LIST_FILE}.")
-
-endif()
-
-endif()
-
-
-if (CONFIG_USE_middleware_vit_fusionf1_models)
-# Add set(CONFIG_USE_middleware_vit_fusionf1_models true) in config.cmake to use this component
-
-message("middleware_vit_fusionf1_models component is included from ${CMAKE_CURRENT_LIST_FILE}.")
-
-if((CONFIG_BOARD STREQUAL evkmimxrt595))
-
-target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
-  ${CMAKE_CURRENT_LIST_DIR}/./FusionF1/Lib
-)
-
-else()
-
-message(SEND_ERROR "middleware_vit_fusionf1_models dependency does not meet, please check ${CMAKE_CURRENT_LIST_FILE}.")
-
-endif()
-
-endif()
-
-
 if (CONFIG_USE_middleware_vit_cm7)
 # Add set(CONFIG_USE_middleware_vit_cm7 true) in config.cmake to use this component
 
 message("middleware_vit_cm7 component is included from ${CMAKE_CURRENT_LIST_FILE}.")
 
-if((CONFIG_BOARD STREQUAL evkmimxrt1040 OR CONFIG_BOARD STREQUAL evkbimxrt1050 OR CONFIG_BOARD STREQUAL evkmimxrt1060 OR CONFIG_BOARD STREQUAL evkbmimxrt1060 OR CONFIG_BOARD STREQUAL evkcmimxrt1060 OR CONFIG_BOARD STREQUAL evkmimxrt1064 OR CONFIG_BOARD STREQUAL evkmimxrt1160 OR CONFIG_BOARD STREQUAL evkmimxrt1170 OR CONFIG_BOARD STREQUAL evkbmimxrt1170))
+if((CONFIG_BOARD STREQUAL evkbmimxrt1060 OR CONFIG_BOARD STREQUAL evkcmimxrt1060 OR CONFIG_BOARD STREQUAL evkbmimxrt1170))
 
 if(CONFIG_TOOLCHAIN STREQUAL mcux)
 target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
@@ -124,37 +26,6 @@ endif()
 else()
 
 message(SEND_ERROR "middleware_vit_cm7 dependency does not meet, please check ${CMAKE_CURRENT_LIST_FILE}.")
-
-endif()
-
-endif()
-
-
-if (CONFIG_USE_middleware_vit_cm33-lpc55s69)
-# Add set(CONFIG_USE_middleware_vit_cm33-lpc55s69 true) in config.cmake to use this component
-
-message("middleware_vit_cm33-lpc55s69 component is included from ${CMAKE_CURRENT_LIST_FILE}.")
-
-if((CONFIG_BOARD STREQUAL lpcxpresso55s69))
-
-if(CONFIG_TOOLCHAIN STREQUAL mcux)
-target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
-  ${CMAKE_CURRENT_LIST_DIR}/./CortexM33-LPC55S69/Lib
-  ${CMAKE_CURRENT_LIST_DIR}/./CortexM33-LPC55S69/Lib/Inc
-)
-endif()
-
-if(CONFIG_TOOLCHAIN STREQUAL mcux)
-  target_link_libraries(${MCUX_SDK_PROJECT_NAME} PRIVATE
-    -Wl,--start-group
-      ${CMAKE_CURRENT_LIST_DIR}/./CortexM33-LPC55S69/Lib/libVIT_CM33_LPC55S69_v04_10_00.a
-      -Wl,--end-group
-  )
-endif()
-
-else()
-
-message(SEND_ERROR "middleware_vit_cm33-lpc55s69 dependency does not meet, please check ${CMAKE_CURRENT_LIST_FILE}.")
 
 endif()
 
